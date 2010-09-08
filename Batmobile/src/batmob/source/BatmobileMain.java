23ef07dec765decbf98a4f5a84a8986aa815d912
cashcif <cashcif@gmail.com>
2010-09-08 12:09:12 +0200

    First version

---
+++ Batmobile/src/batmob/source/BatmobileMain.java
@@ -0,0 +1,13 @@
+package batmob.source;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BatmobileMain extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.foursquare);
+    }
+}
