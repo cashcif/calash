f4ff54e26245068ee221324e03e7ee958b0a6e8b
unknown <wiseash@.(none)>
2010-09-09 23:35:41 +0200

    Original Application

--- Batmobile/src/batmob/source/BatmobileMain.java
+++ Batmobile/src/batmob/source/BatmobileMain.java
@@ -1,13 +1,32 @@
 package batmob.source;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class BatmobileMain extends Activity {
-    /** Called when the activity is first created. */
+	private static final String TAG = "BatmobileMain";
+	/** Called when the activity is first created. */
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.foursquare);
+        setContentView(R.layout.main);
+        
+        Button buttonEnter = (Button) findViewById(R.id.buttonEnter);
+        Log.v(TAG, "Start up screen");
+		buttonEnter.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				Log.v(TAG, "Starting Application");
+				Intent tabs = new Intent(BatmobileMain.this, ButtonGrid.class);
+				BatmobileMain.this.startActivity(tabs);
+				
+			}
+
+		});
     }
 }
---
+++ Batmobile/src/batmob/source/LocationHandler.java
@@ -0,0 +1,67 @@
+//http://www.vogella.de/articles/AndroidLocationAPI/article.html#overview_locationapi
+
+package batmob.source;
+
+import android.app.Activity;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class LocationHandler extends Activity implements LocationListener{
+	
+	private TextView info;
+	private LocationManager locationManager;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.location);
+		info = (TextView) findViewById(R.id.TextView01);
+		locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+		showLocation(locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER));
+		if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER))
+			locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+		if (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER))
+			locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
+	}
+
+	@Override
+	protected void onResume(){
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+	}
+	
+	@Override
+	protected void onPause(){
+		locationManager.removeUpdates(this);
+	}
+
+	@Override
+	public void onLocationChanged(Location loc) {
+		showLocation(loc);
+	}
+	
+	public void showLocation(Location loc) {
+		if (loc != null) {
+			info.setText(loc.toString());
+		} else
+			info.setText("GPS not available");
+}
+	
+	@Override
+	public void onProviderDisabled(String arg0) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void onProviderEnabled(String arg0) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+		//info.setText(arg0 + String.valueOf(arg1) + arg2.toString());
+	}
+
+}
